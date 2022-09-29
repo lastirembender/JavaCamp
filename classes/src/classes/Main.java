4fb51995f34efd2dbc6980c5c8c04415b2e28f4c
İrem <69118634+lastirembender@users.noreply.github.com>
2022-09-29 16:29:01 +0300

    Videos have been watched and applied

---
+++ classes/src/classes/Main.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2 = sayi1;
+		
+		
+	}
+
+}
